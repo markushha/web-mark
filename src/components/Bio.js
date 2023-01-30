4260080ddc42ef9ddcb66383c35f06e89bdfaf8c
Mark <ingerkrut@gmail.com>
2023-01-30 19:43:25 +0600

    bio change

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -5,7 +5,7 @@ function Bio() {
   return (
     <div className="bio-wrapper">
       <div>
-        <img className="bio-img" alt="bio" src="https://media.istockphoto.com/id/1344688156/photo/portrait-of-a-man-using-a-computer-in-a-modern-office.jpg?b=1&s=170667a&w=0&k=20&c=AAVVOvN9uY1Rd_0QsWb8WvBN-4VSUdCORiZx51s5OLc="></img>
+        <img className="bio-img" alt="bio" src="/bio.jpg"></img>
       </div>
       <h1 className="title">Hello, I'm Mark 👋</h1>
       <p className="descr">
@@ -15,7 +15,7 @@ function Bio() {
           steps={[
             "Web Developer 🖥️",
             1000,
-            "Future Entrepreneur 🚀",
+            "Starting Enterpreneur 🚀",
             1000,
             "Basketball Player 🏀",
             1000,
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,8 @@ export default function Navbar() {
 
   return (
     <nav className="wrapper-nav">
-      <div className="nav__item"><p className="nav__item__text"><code>Home</code></p></div>
-      <div className="nav__item"><p className="nav__item__text"><code>Social</code></p></div>
+      <div className="nav__item"><p className="nav__item__text"><code>Me</code></p></div>
+      <div className="nav__item"><p className="nav__item__text"><code>Socials</code></p></div>
       <div className="nav__item"><p className="nav__item__text"><code>Contacts</code></p></div>
     </nav>
   );
